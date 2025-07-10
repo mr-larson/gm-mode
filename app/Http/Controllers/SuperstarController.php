6705b872b0959b0e830ea30ecdb883ed844e81c7
Gauthier <gauthierdewit@gmail.com>
2025-07-10 17:17:15 +0200

    first commit

---
+++ app/Http/Controllers/SuperstarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSuperstarRequest;
+use App\Http\Requests\UpdateSuperstarRequest;
+use App\Models\Superstar;
+
+class SuperstarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSuperstarRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Superstar $superstar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Superstar $superstar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSuperstarRequest $request, Superstar $superstar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Superstar $superstar)
+    {
+        //
+    }
+}
---
+++ app/Models/Superstar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Superstar extends Model
+{
+    /** @use HasFactory<\Database\Factories\SuperstarFactory> */
+    use HasFactory;
+}
